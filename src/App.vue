236a95dd7c9f36f251aadc06eb70efbc26694ded
Muhammadqodir <oHp12TR7FTVA>
2024-09-05 09:00:32 +0500

    feat: some changes

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <header class="product-header">
+    <ProductsHeader />
+  </header>
+  <main class="product-main">
+    <Products :products="products" @to-parent="toParent" />
+  </main>
+  <footer class="product-footer">
+    <ProductsFooter />
+  </footer>
+</template>
+
+<script>
+import Products from "./views/Products.vue";
+import ProductsHeader from "./components/header.vue";
+import ProductsFooter from "./components/footer.vue";
+import "./main.css";
+
+export default {
+  components: { Products, ProductsHeader, ProductsFooter },
+  data() {
+    return {
+      products: [
+        {
+          image: "/src/assets/product.jpg",
+          name: "WEBER RANGLI EMULSIYA",
+          description:
+            "Shokolad rangidagi wb863 suv emulsiyasi. Yuqori sifatli akril kopolimerdan tayyorlangan. Tashqi va ichki ishlar uchun. Oddiy namlik bo'lgan xonalarda devor va shiftlarni bo'yash uchun mo'ljallangan.",
+          price: "250 000 so'm",
+        },
+        {
+          image: "/src/assets/product.jpg",
+          name: "WEBER RANGLI EMULSIYA",
+          description:
+            "Shokolad rangidagi wb863 suv emulsiyasi. Yuqori sifatli akril kopolimerdan tayyorlangan. Tashqi va ichki ishlar uchun. Oddiy namlik bo'lgan xonalarda devor va shiftlarni bo'yash uchun mo'ljallangan.",
+          price: "250 000 so'm",
+        },
+        {
+          image: "/src/assets/product.jpg",
+          name: "WEBER RANGLI EMULSIYA",
+          description:
+            "Shokolad rangidagi wb863 suv emulsiyasi. Yuqori sifatli akril kopolimerdan tayyorlangan. Tashqi va ichki ishlar uchun. Oddiy namlik bo'lgan xonalarda devor va shiftlarni bo'yash uchun mo'ljallangan.",
+          price: "250 000 so'm",
+        },
+        {
+          image: "/src/assets/product.jpg",
+          name: "WEBER RANGLI EMULSIYA",
+          description:
+            "Shokolad rangidagi wb863 suv emulsiyasi. Yuqori sifatli akril kopolimerdan tayyorlangan. Tashqi va ichki ishlar uchun. Oddiy namlik bo'lgan xonalarda devor va shiftlarni bo'yash uchun mo'ljallangan.",
+          price: "250 000 so'm",
+        },
+        {
+          image: "/src/assets/product.jpg",
+          name: "WEBER RANGLI EMULSIYA",
+          description:
+            "Shokolad rangidagi wb863 suv emulsiyasi. Yuqori sifatli akril kopolimerdan tayyorlangan. Tashqi va ichki ishlar uchun. Oddiy namlik bo'lgan xonalarda devor va shiftlarni bo'yash uchun mo'ljallangan.",
+          price: "250 000 so'm",
+        },
+        {
+          image: "/src/assets/product.jpg",
+          name: "WEBER RANGLI EMULSIYA",
+          description:
+            "Shokolad rangidagi wb863 suv emulsiyasi. Yuqori sifatli akril kopolimerdan tayyorlangan. Tashqi va ichki ishlar uchun. Oddiy namlik bo'lgan xonalarda devor va shiftlarni bo'yash uchun mo'ljallangan.",
+          price: "250 000 so'm",
+        },
+      ],
+    };
+  },
+  methods: {
+    toParent(fromChild) {
+      console.log(fromChild);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Products.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <Sections />
+    <div>
+      <Card />
+    </div>
+  </section>
+</template>
+<script>
+import Sections from "../components/sections.vue";
+import Card from "../components/card.vue";
+
+export default {
+  props: ["products"],
+  components: {
+    Sections,
+    Card,
+  },
+  mounted() {
+    this.$emit("to-parent", "Hey parent, from child");
+
+    this.products.forEach((product) => {
+      console.log(product.name);
+    });
+  },
+};
+</script>
+<style></style>
